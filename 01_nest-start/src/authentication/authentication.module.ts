4420ad377b4cbc0d2b8d088014a72cd941c1b729
joonngy <joonnngy@gmail.com>
2023-12-11 22:20:48 +0900

    V0.0.1

---
+++ 01_nest-start/src/authentication/authentication.module.ts
@@ -0,0 +1,30 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { AuthenticationService } from './authentication.service';
+import { AuthenticationController } from './authentication.controller';
+import { LocalStrategy } from './strategy/local.strategy';
+import { JwtStrategy } from './strategy/jwt.strategy';
+import { UsersModule } from '../users/users.module';
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: `${configService.get('JWT_EXPIRATION_TIME')}s`,
+        },
+      }),
+    }),
+  ],
+  providers: [AuthenticationService, LocalStrategy, JwtStrategy],
+  controllers: [AuthenticationController],
+})
+export class AuthenticationModule {}
