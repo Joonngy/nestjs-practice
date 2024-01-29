abce4105628e1846568dbc44cb3b26698f6bbf2f
joonngy <joonnngy@gmail.com>
2024-01-29 20:10:11 +0900

    Chap5 : Serialization

--- 01_nest-start/src/users/entities/user.entity.ts
+++ 01_nest-start/src/users/entities/user.entity.ts
@@ -1,17 +1,21 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-
+import { Expose } from 'class-transformer';
+ 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   public id?: number;
-
+ 
   @Column({ unique: true })
+  @Expose()
   public email: string;
-
+ 
   @Column()
+  @Expose()
   public name: string;
-
+ 
   @Column()
   public password: string;
 }
-
+ 
+export default User;
